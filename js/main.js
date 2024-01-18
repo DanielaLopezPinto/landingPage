fb08c00e97fae050889eb242d3df70a3e7322829
DanielaLopezPinto <152309004+DanielaLopezPinto@users.noreply.github.com.m>
2024-01-18 18:50:37 -0500

    boton de menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 // AOS.init({
 //     offset:1
 // });
+
+//document.addEventListener('DOMContentLoaded', function () {
+//    let nav = document.querySelector('nav');
+//    document.querySelector('.boton').addEventListener('click', function () {
+//      nav.classList.toggle('active');
+//    });
+//  });
