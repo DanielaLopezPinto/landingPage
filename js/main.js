6784b80047af9ddf3e7ab8663bff20d2e43a5511
DanielaLopezPinto <152309004+DanielaLopezPinto@users.noreply.github.com.m>
2024-01-18 21:40:46 -0500

    Menu para celular

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-// AOS.init({
+//AOS.init({
 //     offset:1
-// });
+//});
 
-//document.addEventListener('DOMContentLoaded', function () {
-//    let nav = document.querySelector('nav');
-//    document.querySelector('.boton').addEventListener('click', function () {
-//      nav.classList.toggle('active');
-//    });
-//  });
+document.addEventListener('DOMContentLoaded', function () {
+    let nav = document.querySelector('nav');
+    let menuBtn = document.querySelector('.menu');
+    menuBtn.addEventListener('click', function () {
+        nav.classList.toggle('active');
+    });
+});
