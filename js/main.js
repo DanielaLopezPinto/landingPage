76a5f49468621d3de79563770c8e5fd8ed2ccaa9
DanielaLopezPinto <152309004+DanielaLopezPinto@users.noreply.github.com.m>
2024-01-18 23:11:39 -0500

    correciones

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 //     offset:1
 //});
 
-document.addEventListener('DOMContentLoaded', function () {
-    let nav = document.querySelector('nav');
-    let menuBtn = document.querySelector('.menu');
-    menuBtn.addEventListener('click', function () {
-        nav.classList.toggle('active');
-    });
-});
+//document.addEventListener('DOMContentLoaded', function () {
+//    let nav = document.querySelector('nav');
+//    let menuBtn = document.querySelector('.menu');
+//    menuBtn.addEventListener('click', function () {
+//        nav.classList.toggle('active');
+//    });
+//});
