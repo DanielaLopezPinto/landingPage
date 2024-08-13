0fd2ded56c48ea57e7a7e64158f61fc1ad49d610
Daniela López Pinto <152309004+DanielaLopezPinto@users.noreply.github.com>
2024-08-13 09:18:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-//AOS.init({
-//     offset:1
-//});
+AOS.init({
+     offset:1
+});
 
-//document.addEventListener('DOMContentLoaded', function () {
-//    let nav = document.querySelector('nav');
-//    let menuBtn = document.querySelector('.menu');
-//    menuBtn.addEventListener('click', function () {
-//        nav.classList.toggle('active');
-//    });
-//});
+document.addEventListener('DOMContentLoaded', function () {
+    let nav = document.querySelector('nav');
+    let menuBtn = document.querySelector('.menu');
+    menuBtn.addEventListener('click', function () {
+        nav.classList.toggle('active');
+    });
+});
